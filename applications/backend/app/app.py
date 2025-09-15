ef3bd6caf35425080d7f6c339444f4eb4eabbbc1
Lau Jorge <laujorge1234@gmail.com>
2025-09-15 23:17:15 +0200

    port 8081

--- applications/backend/app/app.py
+++ applications/backend/app/app.py
@@ -78,5 +78,5 @@ def delete_todo(todo_id):
     return {"message": "Task deleted"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 8081))
     app.run(host="0.0.0.0", port=port, debug=False)
