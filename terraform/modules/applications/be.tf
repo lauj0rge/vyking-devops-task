a947915b4d09d7c8c19d14d336822a0b8bc27b57
Lau Jorge <laujorge1234@gmail.com>
2025-09-18 23:36:24 +0200

    refactor

--- terraform/modules/applications/be.tf
+++ terraform/modules/applications/be.tf
@@ -41,12 +41,3 @@ resource "kubernetes_manifest" "backend_app" {
     }
   }
 }
-resource "helm_release" "ingress_nginx" {
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.11.1"
-
-  namespace  = "ingress-nginx"
-  create_namespace = true
-}
